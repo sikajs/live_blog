884d66a4b990e16885244dc5ea3343f369631dce
Jason Shen <sikajs@gmail.com>
2016-08-16 13:59:29 +0800

    Fix typo for omniauth initializer

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,1 +1,1 @@
-OmniAuth.config.full_host = Rails.env.production? ? 'https://live-blog.herokuapps.com' : 'http://localhost:3000'
+OmniAuth.config.full_host = Rails.env.production? ? 'https://live-blog.herokuapp.com' : 'http://localhost:3000'
